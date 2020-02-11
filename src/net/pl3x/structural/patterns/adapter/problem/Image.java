c9a0118afda7802bc36cbfb550670a5e4a976fd9
DwDMadMac <anthonymmacallister@gmail.com>
2020-02-11 11:00:23 -0500

    Adapter Pattern - Problem

--- src/net/pl3x/structural/patterns/adapter/problem/Image.java
+++ src/net/pl3x/structural/patterns/adapter/problem/Image.java
@@ -1,4 +1,9 @@
 package net.pl3x.structural.patterns.adapter.problem;
 
+/*
+ * For demo purposes we won't work with the Image{} that comes with Java
+ *
+ *
+ */
 public class Image {
 }
--- src/net/pl3x/structural/patterns/adapter/problem/avaFilters/Caramel.java
+++ src/net/pl3x/structural/patterns/adapter/problem/avaFilters/Caramel.java
@@ -1,4 +1,24 @@
 package net.pl3x.structural.patterns.adapter.problem.avaFilters;
 
+import net.pl3x.structural.patterns.adapter.problem.Image;
+
+/**
+ * This class will be a filter that is being used for a filter library
+ */
 public class Caramel {
+    /**
+     * This method is required to have by the filter library
+     */
+    public void init(){
+
+    }
+
+    /**
+     * This method is where we render the caramel filter to the image
+     *
+     * @param image Apply caramel filter to image
+     */
+    public void render(Image image){
+        System.out.println("Applying Caramel Filter");
+    }
 }
